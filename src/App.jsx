d8b1258df6893e41de1cce23b7735d7eda223394
Roman <tenerifeopen@gmail.com>
2026-03-22 01:28:50 +0000

    update cards

--- src/App.jsx
+++ src/App.jsx
@@ -266,7 +266,7 @@ export default function App() {
               padding: 20,
               fontSize: "clamp(27px, 7vw, 35px)",
               fontWeight: 500,
-              lineHeight: 1.6
+              lineHeight: 1.7
             }}>
               {show ? current?.answer : current?.question}
             </div>
