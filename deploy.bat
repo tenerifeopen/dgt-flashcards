6433ec331470ac2885151aadde10d4adf6076bc7
Roman <tenerifeopen@gmail.com>
2026-03-23 19:02:42 +0000

    update cards

--- deploy.bat
+++ deploy.bat
@@ -5,15 +5,15 @@ echo ===== DEPLOY START =====
 cd /d %~dp0
 
 echo.
-echo 1. Добавляем изменения...
-git add .
+echo 1. Добавляем ВСЁ...
+git add -A
 
 echo.
 echo 2. Коммит...
 git commit -m "update cards"
 
 echo.
-echo 3. Отправка на GitHub...
+echo 3. Отправка...
 git push
 
 echo.
